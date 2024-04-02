e145dd493be3781ce27b1148d4e4d13ae6cd5a0a
[강위찬][기술개발본부][사원] <wichan@datau.co.kr>
2024-04-02 13:50:35 +0900

    fix: isYn validator npe 수정

--- src/main/java/com/example/demo/common/validator/IsYnValidator.java
+++ src/main/java/com/example/demo/common/validator/IsYnValidator.java
@@ -6,6 +6,8 @@ import jakarta.validation.ConstraintValidatorContext;
 public class IsYnValidator implements ConstraintValidator<IsYn, String> {
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if (s == null) s = "";
+        // 빈 값이어도 매치
         return s.matches("^[YN]?$");
     }
 }
